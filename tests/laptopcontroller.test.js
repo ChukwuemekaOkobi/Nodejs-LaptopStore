5e479eaecef8e2af6c00dfd9fbac0f8e936f44e3
chukwuemekaokobi <emekafrancis006@gmail.com>
2022-01-14 19:38:00 +0200

    added circle CI config

---
+++ tests/laptopcontroller.test.js
@@ -0,0 +1,21 @@
+
+test("test addition", ()=>{
+
+    var a = 34; 
+    var b = 15; 
+
+    const result = a + b; 
+
+    expect(result).toBe(49)
+});
+
+
+test("test subtraction ", ()=>{
+
+    var a = 34; 
+    var b = 15; 
+
+    const result = a - b; 
+
+    expect(result).toBe(19)
+});
